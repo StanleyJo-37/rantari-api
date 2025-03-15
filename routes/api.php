b4c8369e962ce1bf422563b9e3359ea193d00a0d
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-03-15 23:31:52 +0700

    # feat initial commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('/auth')->group(function() {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/products', [ProductController::class, 'getAllProducts']);
+});
